f68e4ef76b1abffdab937010a0cd3cdb7ea3d8c3
Aniket213 <aniketmittal222@gmail.com>
2024-08-27 21:46:35 +0530

    Working on Quadrents in Todays Task Page

--- src/pages/today/index.js
+++ src/pages/today/index.js
@@ -2,6 +2,7 @@ import React,{useEffect} from 'react';
 import {useSelector,useDispatch} from "react-redux";
 import TaskCard from 'component/Task';
 import {gettaskbyfilter} from 'store/taskSlice';
+import Quadrent from './quadrent';
 
 const Today=()=>{
   const{tasksfilter}=useSelector((state)=>state.task);
@@ -21,14 +22,15 @@ const Today=()=>{
     },[]);
 
   return (
-    <div className="today" style={{marginTop:"100px",width:"100%"}}>
-      <h1 style={{marginTop:"20px",marginBottom:"50px",textAlign:"center"}}>Today Task</h1>
-     {
+    <div className="today" style={{marginTop:"90px", width:"100%", padding: '1rem'}}>
+      <p style={{padding: '1rem 2rem', fontSize: '2rem', fontWeight: '500', fontFamily: 'cursive', marginBottom: '1rem', backgroundColor: '#d8f9ff', borderRadius: '5px'}}>Today Task</p>
+     {/* {
       tasksfilter?.today&&tasksfilter?.today.map((task)=>{
         const date=`${task?.date?.day}-${task?.date?.month}-${task?.date?.year}`;
      return  <TaskCard key={task._id} id={task._id} name={task.task_name} due={task.timinng} duedate={date} />
         })
-      }
+      } */}
+      <Quadrent/>
     </div>
   )
 }
---
+++ src/pages/today/quadrent.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './styles/quadrent.css'
+import { Box } from '@mui/material'
+
+const Quadrent = () => {
+  return (
+    <Box sx={{padding: '1rem 2rem', position: 'relative'}}>
+        <Box className="centerbox">
+            
+        </Box>
+        <Box className="quadrentsparentbox">
+            <Box className="quadrentbox quadrentbox1"><Box className="head head1">SAYS</Box></Box>
+            <Box className="quadrentbox quadrentbox2"><Box className="head head2">THINKS</Box></Box>
+            <Box className="quadrentbox quadrentbox3"><Box className="head head3">DOES</Box></Box>
+            <Box className="quadrentbox quadrentbox4"><Box className="head head4">FEELS</Box></Box>
+        </Box>
+    </Box>
+  )
+}
+
+export default Quadrent
